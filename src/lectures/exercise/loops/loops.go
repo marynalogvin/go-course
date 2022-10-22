b1e85a32a21cb745a0148af4921a24eb385fb028
marynalogvin <marusasheva@gmail.com>
2022-10-22 18:43:06 +0300

    Complete demo & exercise loops

--- src/lectures/exercise/loops/loops.go
+++ src/lectures/exercise/loops/loops.go
@@ -2,10 +2,7 @@
 //  Implement the classic "FizzBuzz" problem using a `for` loop.
 //
 //--Requirements:
-//* Print integers 1 to 50, except:
-//  - Print "Fizz" if the integer is divisible by 3
-//  - Print "Buzz" if the integer is divisible by 5
-//  - Print "FizzBuzz" if the integer is divisible by both 3 and 5
+
 //
 //--Notes:
 //* The remainder operator (%) can be used to determine divisibility
@@ -15,4 +12,21 @@ package main
 import "fmt"
 
 func main() {
+	//* Print integers 1 to 50, except:
+	for i := 0; i < 51; i++ {
+		//  - Print "FizzBuzz" if the integer is divisible by both 3 and 5
+		if i%15 == 0 {
+			fmt.Println("FizzBuzz")
+			// - Print "Fizz" if the integer is divisible by 3
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+			// - Print "Buzz" if the integer is divisible by 5
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
+
+	}
+
 }

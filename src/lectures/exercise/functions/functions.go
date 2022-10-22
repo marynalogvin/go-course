ae10154ab6b1d22618b9c536fe1c865b5b2422cd
marynalogvin <marusasheva@gmail.com>
2022-10-22 13:59:28 +0300

    Complete exercise functions

--- src/lectures/exercise/functions/functions.go
+++ src/lectures/exercise/functions/functions.go
@@ -1,24 +1,49 @@
-//--Summary:
-//  Use functions to perform basic operations and print some
-//  information to the terminal.
+// --Summary:
 //
-//--Requirements:
-//* Write a function that accepts a person's name as a function
-//  parameter and displays a greeting to that person.
-//* Write a function that returns any message, and call it from within
-//  fmt.Println()
-//* Write a function to add 3 numbers together, supplied as arguments, and
-//  return the answer
-//* Write a function that returns any number
-//* Write a function that returns any two numbers
-//* Add three numbers together using any combination of the existing functions.
-//  * Print the result
-//* Call every function at least once
-
+//	Use functions to perform basic operations and print some
+//	information to the terminal.
+//
+// --Requirements:
 package main
 
 import "fmt"
 
+//   - Write a function that accepts a person's name as a function
+//     parameter and displays a greeting to that person.
+func greeting(personsName string) {
+	fmt.Println("Hello", personsName)
+}
+
+//   - Write a function that returns any message, and call it from within
+//     fmt.Println()
+func anyMessage() string {
+	return "Hello from anyMessage function"
+}
+
+//   - Write a function to add 3 numbers together, supplied as arguments, and
+//     return the answer
+func add(n1, n2, n3 int) int {
+	return n1 + n2 + n3
+}
+
+// * Write a function that returns any number
+func anyNumber() int {
+	return 101
+}
+
+// * Write a function that returns any two numbers
+func anyTwoNumbers() (int, int) {
+	return 102, 103
+}
+
 func main() {
+	//* Call every function at least once
+	//  * Print the result
+	greeting("Alex")
+	fmt.Println(anyMessage())
+	// * Add three numbers together using any combination of the existing functions.
+	firstNumber, secondNumber := anyTwoNumbers()
+	addThreeNumbers := add(1, 2, 3) + firstNumber + secondNumber
+	fmt.Println("The adding result is", addThreeNumbers)
 
 }

4dcb5ce468116296fc496edc03ec45a4afd01287
marynalogvin <marusasheva@gmail.com>
2022-10-23 23:08:42 +0300

    Complete exercise arrays

--- src/lectures/exercise/arrays/arrays.go
+++ src/lectures/exercise/arrays/arrays.go
@@ -3,21 +3,51 @@
 //  out information about the list.
 //
 //--Requirements:
-//* Using an array, create a shopping list with enough room
-//  for 4 products
-//  - Products must include the price and the name
-//* Insert 3 products into the array
-//* Print to the terminal:
-//  - The last item on the list
-//  - The total number of items
-//  - The total cost of the items
-//* Add a fourth product to the list and print out the
-//  information again
 
 package main
 
 import "fmt"
 
+// - Products must include the price and the name
+type Product struct {
+	name  string
+	price int
+}
+
+//   - Using an array, create a shopping list with enough room
+//     for 4 products
+//   - Print to the terminal:
+//   - The last item on the list
+//   - The total cost of the items
+//   - The total number of items
+
+func printInfo(shoppingList [4]Product) {
+	var totalCost, totalItems int
+
+	for i := 0; i < len(shoppingList); i++ {
+		item := shoppingList[i]
+		totalCost += item.price
+
+		if item.name != "" {
+			totalItems += 1
+		}
+	}
+	fmt.Println("The last item on the list is", shoppingList[totalItems-1])
+	fmt.Println("The total cost of the items", totalCost)
+	fmt.Println("The total number of items", totalItems)
+}
+
 func main() {
+	//* Insert 3 products into the array
+	shoppingList := [4]Product{
+		{"Milk", 5},
+		{"Bread", 2},
+		{"Eggs", 6},
+	}
+	printInfo(shoppingList)
+	//   - Add a fourth product to the list and print out the
+	//     information again
+	shoppingList[3] = Product{"Oranges", 4}
+	printInfo(shoppingList)
 
 }

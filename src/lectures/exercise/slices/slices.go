2d0bc3c56f2ffe1cc9760f2baeb7650351468017
marynalogvin <marusasheva@gmail.com>
2022-10-26 18:53:22 +0300

    Complete exercise slices

--- src/lectures/exercise/slices/slices.go
+++ src/lectures/exercise/slices/slices.go
@@ -1,16 +1,15 @@
-//--Summary:
-//  Create a program to manage parts on an assembly line.
+// --Summary:
 //
-//--Requirements:
-//* Using a slice, create an assembly line that contains type Part
-//* Create a function to print out the contents of the assembly line
-//* Perform the following:
-//  - Create an assembly line having any three parts
-//  - Add two new parts to the line
-//  - Slice the assembly line so it contains only the two new parts
-//  - Print out the contents of the assembly line at each step
-//--Notes:
-//* Your program output should list 3 parts, then 5 parts, then 2 parts
+//	Create a program to manage parts on an assembly line.
+//
+// --Requirements:
+// * Using a slice, create an assembly line that contains type Part
+// * Create a function to print out the contents of the assembly line
+// * Perform the following:
+//   - Print out the contents of the assembly line at each step
+//
+// --Notes:
+// * Your program output should list 3 parts, then 5 parts, then 2 parts
 
 package main
 
@@ -18,5 +17,24 @@ import "fmt"
 
 type Part string
 
+func printParts(assemblyLine []Part) {
+	for i := 0; i < len(assemblyLine); i++ {
+		element := assemblyLine[i]
+		fmt.Println(element)
+	}
+}
 func main() {
+	//   - Create an assembly line having any three parts
+	assemblyLine := []Part{"CleanDetails", "VarifyDetails", "AssembleDetails"}
+	fmt.Println("3 parts:")
+	printParts(assemblyLine)
+	//   - Add two new parts to the line
+	assemblyLine = append(assemblyLine, "PackDetails", "UnloadDetails")
+	fmt.Println("\n Added 2 parts:")
+	printParts(assemblyLine)
+	//   - Slice the assembly line so it contains only the two new parts
+	assemblyLine = assemblyLine[3:]
+	fmt.Println("\n Sliced:")
+	printParts(assemblyLine)
+
 }

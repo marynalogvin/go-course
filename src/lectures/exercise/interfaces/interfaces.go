1ba2cc961860eff237875bdde95a5c039a7cb111
marynalogvin <marusasheva@gmail.com>
2022-11-01 10:45:09 +0200

    Complete exercise interfaces

--- src/lectures/exercise/interfaces/interfaces.go
+++ src/lectures/exercise/interfaces/interfaces.go
@@ -3,16 +3,7 @@
 //  to the correct vehicle lift, based on vehicle size.
 //
 //--Requirements:
-//* The shop has lifts for multiple vehicle sizes/types:
-//  - Motorcycles: small lifts
-//  - Cars: standard lifts
-//  - Trucks: large lifts
-//* Write a single function to handle all of the vehicles
-//  that the shop works on.
-//* Vehicles have a model name in addition to the vehicle type:
-//  - Example: "Truck" is the vehicle type, "Road Devourer" is a model name
-//* Direct at least 1 of each vehicle type to the correct
-//  lift, and print out the vehicle information.
+
 //
 //--Notes:
 //* Use any names for vehicle models
@@ -21,6 +12,57 @@ package main
 
 import "fmt"
 
+// * The shop has lifts for multiple vehicle sizes/types:
+//   - Motorcycles: small lifts
+//   - Cars: standard lifts
+//   - Trucks: large lifts
+
+type LiftPicker interface {
+	PickLift()
+}
+
+type Motorcycle string
+type Car string
+type Truck string
+
+// * Vehicles have a model name in addition to the vehicle type:
+//   - Example: "Truck" is the vehicle type, "Road Devourer" is a model name
+
+func (m Motorcycle) String() string {
+	return fmt.Sprintf("motorcycle: %v", string(m))
+}
+func (c Car) String() string {
+	return fmt.Sprintf("car: %v", string(c))
+}
+func (t Truck) String() string {
+	return fmt.Sprintf("truck: %v", string(t))
+}
+
+func (m Motorcycle) PickLift() {
+	fmt.Printf("send %v to small lift\n", m)
+}
+func (c Car) PickLift() {
+	fmt.Printf("send %v to standard lift\n", c)
+}
+func (t Truck) PickLift() {
+	fmt.Printf("send %v to large lift\n", t)
+}
+
+//   - Write a single function to handle all of the vehicles
+//     that the shop works on.
+
+func sendToLift(p LiftPicker) {
+	p.PickLift()
+
+}
 func main() {
+	//* Direct at least 1 of each vehicle type to the correct
+	//  lift, and print out the vehicle information.
+	motorcycle := Motorcycle("Retro")
+	car := Car("Sporty")
+	truck := Truck("Fire truck")
+	sendToLift(motorcycle)
+	sendToLift(car)
+	sendToLift(truck)
 
 }

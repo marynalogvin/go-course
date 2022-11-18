4fe4d2ae7832f4bdd984949920000628822f9251
marynalogvin <marusasheva@gmail.com>
2022-11-18 16:53:34 +0200

    Complete reader exercise

--- src/lectures/exercise/reader/reader.go
+++ src/lectures/exercise/reader/reader.go
@@ -18,8 +18,53 @@
 
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
 
 func main() {
+	r := bufio.NewReader(os.Stdin)
+	sumNonBlankLines := 0
+	sumCommands := 0
 
+	i := true
+	for i {
+		input, inputErr := r.ReadString('\n')
+		inputWord := strings.Trim(input, "\n")
+
+		switch {
+		//* When the user enters either "hello" or "bye", the program
+		//  should respond with a message after pressing the enter key.
+		case inputWord == "hello":
+			fmt.Println("Hello from bufio")
+			sumCommands += 1
+			sumNonBlankLines += 1
+		case inputWord == "bye":
+			fmt.Println("Bye from bufio")
+			sumCommands += 1
+			sumNonBlankLines += 1
+		case inputWord == "Q", inputWord == "q":
+			//* Whenever the user types a "Q" or "q", the program should exit.
+			fmt.Println("Exit")
+			i = false
+		default:
+			inputWord = strings.TrimSpace(inputWord)
+			if inputWord != "" {
+				sumNonBlankLines += 1
+			}
+		}
+
+		if inputErr == io.EOF {
+			break
+		}
+		if inputErr != nil {
+			fmt.Println("Error reading Stdin:", inputErr)
+		}
+	}
+	fmt.Printf("The number of non-blank lines entered: %v\n", sumNonBlankLines)
+	fmt.Printf("The number of commands entered: %v\n", sumCommands)
 }
